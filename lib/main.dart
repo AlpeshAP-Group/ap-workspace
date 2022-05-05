50babd36a84fe040e04d785d786b270acab80d9c
Darshan <darshanp.apgroup@email.com>
2022-05-05 19:34:51 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       }
     });
-    // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+// FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
     //   Navigator.pushNamed(
     //     context,
     //     '/message',
